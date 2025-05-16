614f6779147a8c0ca89dd20e6f7187aec749928c
Diego Lima <diegolima20100@gmail.com>
2025-05-16 18:40:34 -0300

    feat: validando as depesas usando if

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,10 +1,6 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
 {
@@ -14,6 +10,35 @@ namespace CashFlow.Application.UseCases.Expenses.Register
         public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
         {
             return new ResponseRegisteredExpenseJson();
+        }
+
+        private void validate(RequestRegisterExpenseJson request)
+        {
+            var titleIsEmpty = string.IsNullOrEmpty(request.Title);
+
+            if (titleIsEmpty) 
+            {
+                throw new ArgumentException("The title is required.");
+            }
+
+            if(request.Amount <= 0)
+            {
+                throw new ArgumentException("The Amount must be greated than zero.");
+            }
+
+            var compareDate = DateTime.Compare(request.Date, DateTime.UtcNow);
+
+            if (compareDate < 0) 
+            {
+                throw new ArgumentException("Expenses cannot be for the future.");
+            }
+
+            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+
+            if (!paymentTypeIsValid) 
+            {
+                throw new ArgumentException("Payment Type is not valid.");
+            }
         }
     }
 }
