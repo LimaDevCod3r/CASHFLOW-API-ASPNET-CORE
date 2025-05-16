2f968dd1c8bef8c666894572212e5c0ec7c19864
Diego Lima <diegolima20100@gmail.com>
2025-05-16 19:42:01 -0300

    feat: Recuperando listas de erros

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -20,6 +20,15 @@ namespace CashFlow.Application.UseCases.Expenses.Register
             var validator = new RegisterExpenseValidator();
 
             var result = validator.Validate(request);
+
+
+            if (!result.IsValid)
+            {
+                var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+                throw new ArgumentException();
+            }
+
+
         }
     }
 }
