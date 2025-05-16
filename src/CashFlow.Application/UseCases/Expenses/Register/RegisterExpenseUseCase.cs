7cff9541ee514d7646651c774b39f04fc9779220
Diego Lima <diegolima20100@gmail.com>
2025-05-16 18:27:58 -0300

    feat: adiciona a regra de negócio

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseUseCase
+    {
+
+        public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+        {
+            return new ResponseRegisteredExpenseJson();
+        }
+    }
+}
---
+++ src/CashFlow.Communication/Responses/ResponseRegisteredExpenseJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Communication.Responses
+{
+    public class ResponseRegisteredExpenseJson
+    {
+        private string Title {  get; set; } = string.Empty;
+
+    }
+}
